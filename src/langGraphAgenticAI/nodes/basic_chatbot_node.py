e0c98d6c3964bfb95b2f84d27762d404c1b9b20d
goswamiayush201 <goswamiayush201@gmail.com>
2025-10-27 18:38:29 +0530

    Fixed Bugs

---
+++ src/langGraphAgenticAI/nodes/basic_chatbot_node.py
@@ -0,0 +1,15 @@
+from src.langGraphAgenticAI.state.state import State
+
+class BasicChatbotNode:
+    """
+    Basic Chatbot login implementation
+    """
+    def __init__(self,model):
+        self.llm=model
+
+    def process(self,state:State)->dict:
+        """
+        Processes the input state and generates a chatbot response.
+        """
+        return {"message":self.llm.invoke(state['message'])}
+
